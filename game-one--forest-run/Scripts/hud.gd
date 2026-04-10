056df469a8b5d1d4e666e64442aa06bba4809b74
Vania <kuimov.ivan@proton.me>
2026-04-10 16:18:20 +0200

    attributes

--- game-one--forest-run/Scripts/hud.gd
+++ game-one--forest-run/Scripts/hud.gd
@@ -3,9 +3,10 @@ extends CanvasLayer
 var fps
 
 func updateMaxHealth():
-	print(GScript.playerStat.maxHealth)
-	set_health_bar()
+	#print(GScript.playerStat.maxHealth)
 	$HealthBar.max_value = GScript.playerStat.maxHealth
+	$health.text = "Max health: " + str(GScript.playerStat.maxHealth)
+	set_health_bar()
 
 func _process(delta: float) -> void:
 	set_health_bar()
@@ -21,20 +22,21 @@ func _ready() -> void:
 	GScript.update.connect(updateMaxHealth)
 	set_health_bar()
 	$HealthBar.max_value = GScript.playerStat.maxHealth
+	$health.text = "Max health: " + str(GScript.playerStat.maxHealth)
 	self.visible = false
 	
 	
 func set_health_bar() -> void:
 	$HealthBar.value = GScript.playerStat.health
-	if GScript.playerStat.health >= 50: 
+	if GScript.playerStat.health >= $HealthBar.max_value/2: 
 		var new_style = $HealthBar.get_theme_stylebox("fill")
 		new_style.bg_color = Color8(0, 137, 30)
 		$HealthBar.add_theme_stylebox_override("fill", new_style)
-	if GScript.playerStat.health < 50 and GScript.playerStat.health >= 25: 
+	if GScript.playerStat.health < $HealthBar.max_value/2 and GScript.playerStat.health >= $HealthBar.max_value/4: 
 		var new_style = $HealthBar.get_theme_stylebox("fill")
 		new_style.bg_color = Color8(242, 139, 78)
 		$HealthBar.add_theme_stylebox_override("fill", new_style)
-	if GScript.playerStat.health < 25: 
+	if GScript.playerStat.health < $HealthBar.max_value/4: 
 		var new_style = $HealthBar.get_theme_stylebox("fill")
 		new_style.bg_color = Color8(213, 0, 26)
 		$HealthBar.add_theme_stylebox_override("fill", new_style)
--- game-one--forest-run/global.gd
+++ game-one--forest-run/global.gd
@@ -9,38 +9,40 @@ var time
 
 var hud_open:bool
 
-var playerStat:Dictionary = {'health':100, 'maxHealth':100, 'agility':50, 'weight':70}
+var playerStat:Dictionary = {'health':100.0, 'maxHealth':100.0, 'agility':50.0, 'weight':70.0}
 
 signal update
 
 #global function to change player attributes
 #time set to 999 = permanent upgrade
-func attribute(changes:Dictionary, time:int=999):
-	var old:Dictionary = playerStat
+func attribute(changes:Dictionary, timerTime:int=999):
+	var save = playerStat.duplicate()
+	print(playerStat)
 	update.emit()
-	if changes.health != 0:
-		playerStat.health *= changes.health
+	if changes.has("maxHealth"):
+		playerStat.maxHealth = playerStat.maxHealth + playerStat.maxHealth*(changes.maxHealth/100)
+	if changes.has("health"):
+		playerStat.health = playerStat.health + playerStat.health*(changes.health/100)
 		clamp(playerStat.Health, 0, playerStat.maxHealth)
-	if changes.maxHealth != 0:
-		playerStat.maxHealth *= changes.maxHealth
-	if changes.agility != 0:
-		playerStat.agility *= changes.agility
-	if changes.weight != 0:
-		playerStat.weight *= changes.weight
+	if changes.has("agility"):
+		playerStat.agility = playerStat.agility + playerStat.agility*(changes.agility/100)
+	if changes.has("weight"):
+		playerStat.weight = playerStat.weight + playerStat.weight*(changes.weight/100)
+	print(playerStat)
 	if time != 999:
-		var t = Timer.new()
-		t.wait_time = time
-		t.start()
-		await t.timeout
-		playerStat = old
+		await get_tree().create_timer(timerTime).timeout
+		playerStat = save
+		print(playerStat)
 		update.emit()
 	return
-	
-	
+
 #function to check singal
-#func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("hud"):
-		#playerStat.maxHealth += 2
-		#update.emit()
-	
-	
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("hud"):
+		#create a dictionary to pass to the function
+		#keys have to correspond to keys in playerStat dict
+		#values are percentages
+		#values have to be floats
+		var attributes:Dictionary = {'maxHealth':20.0, 'agility':20.0}
+		#dont set time if you want upgrades permanent
+		attribute(attributes, 2)
